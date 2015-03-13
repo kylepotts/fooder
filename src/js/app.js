fc067280f2c3559cc722ca0a9d157a60bae03032
Kyle <kyle@kptechblog.com>
2015-03-13 15:55:41 -0500

    init commit

--- src/js/app.js
+++ src/js/app.js
@@ -7,52 +7,32 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
-});
 
-main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
+
+
+var courtMenu = new UI.Menu({
+  sections: [{
+    items: [{
+      title: 'Ford',
+    }, {
+      title: 'Wiley',
+    },{
+      title:'Earhart',
+    },{
+      title:"Windsor",
+    },{
+      title:"Hillenbrand"
     }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+  }]
 });
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+courtMenu.on('select',onCourtSelect)
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+
+function onCourtSelect(e){
+  console.log("Selected court " + e.item.title)
+}
+
+
+courtMenu.show()
